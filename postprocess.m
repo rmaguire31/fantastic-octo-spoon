5a09dd11fd544dd8f05b66b09559962bb546749d
Lauren Miller <laurenmiller@Laurens-MacBook-Pro-2.local>
2016-10-17 11:44:00 +0100

    "INITIAL "

--- postprocess.m
+++ postprocess.m
@@ -12,7 +12,7 @@
 %  f - Domain
 %  P1 - Spectrum
 
-function [f,P1] = process(data1,Fs)
+function [f,P1] = postprocess(data1,Fs)
 
 L = length(data1); % Length of signal 
 T = 1/Fs; % Time period 
@@ -23,6 +23,9 @@ P1 = P2(1:L/2+1); % Compute single-sided spectrum
 P1(2:end-1) = 2*P1(2:end-1); % Compute even-valued signal length L
 f = Fs*(0:(L/2))/L; % Define frequency domain
 plot(f,P1) % Display spectrum
+title('Single-Sided Amplitude Spectrum of Time Series')
+xlabel('f (Hz)')
+ylabel('|P1(f)|')
 
 end
 
