b141719f9aff0be502e3bcaabd6a00fb86bdb2d3
laurenmiller <laurenmiller@laurens-mbp-2>
2016-11-01 18:34:24 +0000

    lauren 1/11/16

--- resample.m
+++ resample.m
@@ -2,5 +2,15 @@
 %
 %  DESCRIPTION
 %   Allow off-line captured data resampling
+%  INPUTS
+%   ts - Time series object
+%   time - Time vector utilised to resample the time series object
+%   interp_method - Interpolation method (linear)
+%  OUTPUTS
+%   ts1 -  New time series object
 
+ts1 = resample(ts, time, interp_method); % Conduct resampling
 
+ts1.time % Display time, data and interpolation method
+ts1.data
+ts1.getinterpmethod
