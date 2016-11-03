0780ae26e4d207cf441fb6482b15d923c2a9c9da
laurenmiller <laurenmiller@student-10-245-130-244.wlan.dur.ac.uk>
2016-11-03 16:58:57 +0000

    lauren resample

--- resample.m
+++ resample.m
@@ -1,13 +1,17 @@
-%% Resampling and Interpolation
+%% RESAMPLE
 %
-%  DESCRIPTION
+% DESCRIPTION
 %   Allow off-line captured data resampling
-%  INPUTS
+%
+% INPUTS
 %   ts - Time series object
 %   time - Time vector utilised to resample the time series object
 %   interp_method - Interpolation method (linear)
-%  OUTPUTS
+%
+% OUTPUTS
 %   ts1 -  New time series object
+%
+% COPYRIGHT (C) Lauren Miller 2016
 
 ts1 = resample(ts, time, interp_method); % Conduct resampling
 
