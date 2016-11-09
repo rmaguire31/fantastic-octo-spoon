ab3af50ad2cc8538ffd424e50b7c4d0900114bcd
rmaguire31 <rmaguire31@gmail.com>
2016-11-09 03:55:06 +0000

    "Moved get_config(). Handle a few more error cases. Tidy up"

--- set_config.m
+++ set_config.m
@@ -19,13 +19,27 @@ for i = 1:length(config)
 end
 
 % Find out how many captures the user would like to schedule.
-inp = inputdlg(...
-    {'Number of Captures:'}, 'Settings', [1 40],...
-    {num2str(length(config))}...
-);
-num_captures = 0;
-if ~isempty(inp)
-    num_captures = str2double(inp{1});
+exit_flag = false;
+while ~exit_flag
+    inp = inputdlg(...
+        {'Number of Captures:'}, 'Settings', [1 40],...
+        {num2str(length(config))}...
+    );
+    num_captures = [];
+    if ~isempty(inp)
+        num_captures = str2double(inp{1});
+    end
+    
+    % Check input.
+    exit_flag = ...
+        isempty(num_captures) ||...
+        ~rem(num_captures, 1) && num_captures > 0;
+    if ~exit_flag
+        uiwait(errordlg(...
+            'Number of captures should be a positive integer.',...
+            'Incorrect Value Entered'...
+        ));
+    end
 end
 
 % Choose sensible defaults, based on current settings file.
@@ -36,19 +50,18 @@ if num_captures > length(config)
         config{i}.start_time_s = config{i-1}.end_time_s + 1;
         config{i}.end_time_s = config{i}.start_time_s + 2;
     end
-else
+elseif num_captures < length(config)
     config = config(1:num_captures);
 end
 
-if num_captures == 1
-    % Prompt.
-    config{1} = prompt_capture(config{1});
-elseif num_captures > 1
-    % Keep prompting until user presses Apply (~edit).
-    edit = true;
-    while edit
-        [config, edit] = prompt_config(config);
-        if ~edit
+if num_captures > 0
+    % Keep prompting until user presses Done.
+    done = false;
+    while ~done
+        [config, done] = prompt_config(config);
+        
+        % Check for overlapping captures.
+        if ~done
             for i = 2:length(config)
                 if config{i}.start_time_s <= config{i-1}.end_time_s
                     msg = sprintf(...
@@ -56,15 +69,15 @@ elseif num_captures > 1
                         i, i - 1 ...
                     );
                     uiwait(errordlg(msg, 'Overlapping Captures'));
-                    edit = true;
+                    done = true;
                 end
             end
         end
     end
 end
 
-% Use length, not end time as this is easier for the microcontroller to
-% parse.
+% Convert back to length, not end time as this is easier for the
+% microcontroller to parse.
 for i = 1:length(config)
     config{i}.length = floor(...
         (config{i}.end_time_s - config{i}.start_time_s) *...
@@ -72,35 +85,68 @@ for i = 1:length(config)
     );
     config{i} = rmfield(config{i}, 'end_time_s');
 end
+% Write config file.
 if ~isempty(config)
-    % Write config file.
     jsondump(config, config_fname);
 end
 end
 
-function [config, edit] = prompt_config(config)
+function config = get_config(config_fname)
+%% GET_CONFIG
+%
+%
+%
+
+try
+    % Parse existing config file.
+    config = jsonload(config_fname);
+catch e
+    module = e.identifier(1:strfind(e.identifier, ':')-1);
+    switch module
+        case {'jsonload'}
+            % Create new config, populated with some default values.
+            config = {};
+            config{1} = struct();
+            config{1}.start_time_s = 5;
+            config{1}.sample_rate_hz = 44.1;
+            config{1}.length = 200;
+            jsondump(config, config_fname);
+        otherwise
+            rethrow(e);
+    end
+end
+end
+
+function [config, done] = prompt_config(config)
 %% PROMPT_CONFIG
 %
 %
 %
 
+% Display capture details as prompts, makes it easier for user to review
+% configuration.
 prompts = cell(length(config), 1);
 for i = 1:length(prompts)
     prompts{i} = sprintf(...
-        'Start: %d, End: %d, Rate: %d',...
+        'Start: %.3g s, End: %.3g s, Rate: %.3g Hz',...
         config{i}.start_time_s,...
         config{i}.end_time_s,...
         config{i}.sample_rate_hz...
     );
 end
 
+% Prompt user for capture to edit.
 [idx, edit] = listdlg(...
+    'Name', 'Settings',...
+    'PromptString', 'Scheduled Captures:',...
     'ListString', prompts,...
     'OKString', 'Edit',...
-    'CancelString', 'Apply',...
+    'CancelString', 'Done',...
     'SelectionMode', 'single',...
-    'ListSize', [220, 300]...
+    'ListSize', [300, 300]...
 );
+% If the user didn't press Edit, he pressed Done and vice versa.
+done = ~edit;
 if edit && idx <= length(config)
     config{idx} = prompt_capture(config{idx});
 end
@@ -108,16 +154,18 @@ end
 % Sort config by start time.
 sorted_idxs = [];
 for i = 1:length(config)
+    % Exclude already sorted indices.
     unsorted_idxs = setdiff(1:length(config), sorted_idxs);
     
-    % Find next smallest value.
+    % Find index of next smallest value.
     sorted_idxs(i) = unsorted_idxs(1);
-    for j = unsorted_idxs
+    for j = unsorted_idxs(2:end)
         if config{j}.start_time_s < config{sorted_idxs(i)}.start_time_s
             sorted_idxs(i) = j;
         end
     end
 end
+% Permute config.
 config = config(sorted_idxs);
 end
 
@@ -166,6 +214,15 @@ while (exit_flagS == 0)
                 'Incorrect Value Entered'...
             ));
             exit_flagS = 0;
+        elseif capture.start_time_s < 5
+            % Warn if start time is earlier than two seconds.
+            uiwait(warndlg(...
+                [
+                    'The microcontroller may miss the start of the '...
+                    'capture due to scheduling process time.'
+                ],...
+                'Early Start Time'...
+            ));
         end
 
         % Make sure End Capture time is larger than the Start Capture Time
@@ -187,32 +244,6 @@ while (exit_flagS == 0)
         end
     else
         capture = default_capture;
-    end
-end
-end
-
-function config = get_config(config_fname)
-%% GET_CONFIG
-%
-%
-%
-
-try
-    % Parse existing config file.
-    config = jsonload(config_fname);
-catch e
-    module = e.identifier(1:strfind(e.identifier, ':')-1);
-    switch module
-        case {'jsonload'}
-            % Write new config file, populated with some default values.
-            config = {};
-            config{1} = struct();
-            config{1}.start_time_s = 5;
-            config{1}.sample_rate_hz = 100;
-            config{1}.length = 200;
-            jsondump(config, config_fname);
-        otherwise
-            rethrow(e);
     end
 end
 end
